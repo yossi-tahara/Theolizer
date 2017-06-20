430f743782dd8e961388a3ff5c7e6a32dc3d842c
yossi-tahara <yossi-tahara@theolizer.com>
2017-06-20 16:57:44 +0900

    ドライバのリプレースをやめたのでmsvc用のreplace/resotre.batが不要になったので削除 ------ auto generated message by Theolizer auto test(one-set) : PASS TheolizerDriver  : b2470e17606f268ed4fdd81aee59c066 TheolizerLibrary : fb3151ac7e39230501194492108bd613 Library's Header : 373c3c8571f1a3a5145eb82c9d090106 ------ end of message

--- source/driver/helper/msvc/restore.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-call bin\impl.bat "%VSSDK140Install%" restore
-call bin\impl.bat "%VSSDK140Install%" restore amd64
-call bin\impl.bat "%VSSDK140Install%" restore amd64_x86
-call bin\impl.bat "%VSSDK140Install%" restore x86_amd64
-
-pause
