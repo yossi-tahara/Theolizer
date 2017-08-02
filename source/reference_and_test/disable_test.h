abb3c41e77aa22c89abcb75bd7ff462d9196d7f3
yossi-tahara <yossi-tahara@theolizer.com>
2017-08-03 04:02:37 +0900

    全テスト有効 ------ auto generated message by Theolizer TheolizerDriver  : e2fdb825d27c41be3c85e54bef1f525b TheolizerLibrary : cfb0a8db795fe1458fc605a3c7e1adef Library's Header : 9f2c7b6f60eef1af19d6077142b10da0 ------ end of message

--- source/reference_and_test/disable_test.h
+++ source/reference_and_test/disable_test.h
@@ -46,7 +46,7 @@
 // ***************************************************************************
 
 #ifndef BUILD_ERROR_TEST
-  #if 0
+  #if 1
     #define ENABLE_BASIC_PROCESS_TEST
     #define ENABLE_CLASS_VARIATION_TEST
         #define ENABLE_SINGLE_TEST
